e5ea046eee7b8a2fcb4e71d2e1cba0dd6b635ae3
Aastha Gupta <aastha.gupta411@gmail.com>
2017-03-30 15:16:27 +0530

    changed proc file

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 # @Author: Aastha Gupta
 # @Date:   2017-03-30 13:16:19
 # @Last Modified by:   Aastha Gupta
-# @Last Modified time: 2017-03-30 13:53:59
+# @Last Modified time: 2017-03-30 15:15:13
 
 from flask import Flask
 
@@ -11,4 +11,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('guest-login.html')
+	return 'Hello World! :D'
